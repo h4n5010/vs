5b87d1160a9c186d603191279c8cbccba3ba0b60
Florian Meier <flo-benutzerkonten@posteo.de>
2020-05-13 11:42:27 +0200

    Removed task1 files from root directory

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "prak1.h"
-
-int main(){
-
-	/* task praktikum 1 */
-	create_process_fork();
-	return 0;
-}
--- prak1.h
+++
@@ -1,3 +0,0 @@
-#define NUMBER_OF_PROCESSES 3 
-
-void create_process_fork();
