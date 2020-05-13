89864005107ffcfbf765855924a498577cba89f8
Florian Meier <flo-benutzerkonten@posteo.de>
2020-05-13 11:38:56 +0200

    Created folder for task 1

---
+++ vs1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "prak1.h"
+
+int main(){
+
+	/* task praktikum 1 */
+	create_process_fork();
+	return 0;
+}
---
+++ vs1/prak1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "prak1.h"
+
+pid_t waitpid(pid_t pid, int *status, int ops);
+
+
+void create_process_fork(){
+
+	int pid[NUMBER_OF_PROCESSES];
+	int status;
+
+	for (int process = 0; process < NUMBER_OF_PROCESSES; process++){
+		switch(pid[process] =  fork()) {
+			case -1:
+				perror("Fork failed!!");
+				exit(1);
+			case 0:
+				/* child process */
+				printf("child process %d (%d) entered critical state!\n", process, getpid());
+				sleep(1);
+				printf("child process %d (%d) leaved critical state!\n", process, getpid());
+				sleep(1);
+			default: 
+				/* father */
+				waitpid(pid[process], &status, 0);
+
+		}
+	}
+	printf("father process has stopped!\n");
+
+}
