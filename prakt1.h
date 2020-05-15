83480950703b10b42c0770d137dbec3037e46cb1
Florian Meier <flo-benutzerkonten@posteo.de>
2020-05-15 14:42:13 +0200

    Changed directory of home in header

--- prakt1.h
+++ prakt1.h
@@ -4,4 +4,4 @@ void init_sem();
 void create_process_fork();
 void V(int sem_num);
 void P(int sem_num);
-static const char HOME[] = "/mnt/d/PersonalThings/FFI/FFI-Semester/Semester 4/Verteilte Systeme/VSPraktikum/vs/vs1";
+static const char HOME[] = "/home/florian/vs";
