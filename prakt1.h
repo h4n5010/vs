6535f76c50a0e8c288e8551aec565f3d9d9099ef
Florian Meier <flo-benutzerkonten@posteo.de>
2020-05-15 17:08:56 +0200

    Deleted old Task1 Source files. See branch vs1

--- prakt1.h
+++
@@ -1,7 +0,0 @@
-#define NUMBER_OF_PROCESSES 3 
-
-void init_sem();
-void create_process_fork();
-void V(int sem_num);
-void P(int sem_num);
-static const char HOME[] = "/mnt/d/PersonalThings/FFI/FFI-Semester/Semester 4/Verteilte Systeme/VSPraktikum/vs/vs1";
