830122dd4ba8429c2665a49bef942c30f9fb38e3
Valentin Röckseisen <valentin.roeckseisen@gmail.com>
2020-05-15 14:07:10 +0200

    Commit

--- prakt1.h
+++ prakt1.h
@@ -4,9 +4,4 @@ void init_sem();
 void create_process_fork();
 void V(int sem_num);
 void P(int sem_num);
-<<<<<<< HEAD:vs1/prakt1.h
 static const char HOME[] = "/mnt/d/PersonalThings/FFI/FFI-Semester/Semester 4/Verteilte Systeme/VSPraktikum/vs/vs1";
-=======
-static const char HOME[] = "/home/pi/vs/";
-
->>>>>>> 863c827a5af0aba1ae8db13c6897573ace5f2fce:prakt1.h
