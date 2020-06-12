aba8e49fbad7d674050c7bf38f9b0d70e40be614
Florian Meier <flo-benutzerkonten@posteo.de>
2020-06-12 14:22:16 +0200

    Ka

--- client.c
+++ client.c
@@ -13,14 +13,14 @@ int main(){
 	numbers->a = 5;
 	numbers->b = 6;
 
-	int * cube = 5;
+	int cube = 5;
 
 	int * result = add_1(numbers, c1);
 	printf("Addition: %d\n", *result);
 	int * result1 = multiply_1(numbers, c1);
 	
 	printf("Multiply: %d\n", *result);
-	int * result2 = cube_1(cube, c1);
+	int * result2 = cube_1(&cube, c1);
 
 	printf("Cube: %d\n", *result);
 }
