55e22f5be6f884681f5d747a2d03a72b42d5d561
Florian Meier <flo-benutzerkonten@posteo.de>
2020-06-13 15:41:32 +0200

    Getter und Setter synchronisiert (1c)

--- src/ConcreteFigur.java
+++ src/ConcreteFigur.java
@@ -1,14 +1,14 @@
 public class ConcreteFigur extends Figur {
 
     // Neue Koordinaten fuer Schachfigur setzen
-    public void setPosition(char x, int y) {
+    public synchronized void setPosition(char x, int y) {
         this.x = x;
         MachMal.eineZehntelSekundeLangGarNichts(); // 10 Millisekunden warten gemaeß Aufgabenstellung
         this.y = y;
     }
 
     // Gibt die aktuelle Position der Schachfigur als String zurück
-    public String getPosition(){
+    public synchronized String getPosition(){
         for(int i = 0; i < 10; i++) { // Eine Sekunde lang warten gemaeß Aufgabenstellung
             MachMal.eineZehntelSekundeLangGarNichts();
         }
