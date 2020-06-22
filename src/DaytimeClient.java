81fff0acef0a3cd88832a2bcb836a70470a21c50
florian <aksdfasodfhja>
2020-06-22 16:27:20 +0200

    Namen geaendert

--- src/DaytimeClient.java
+++ src/DaytimeClient.java
@@ -5,14 +5,13 @@ import java.util.Date;
 public class DaytimeClient {
     public static void main(String[] args) throws Exception{
         String host = args[0];
-        //String host = "Test"; // Nur für Debugging selbst gesetzt, kann kein argument in intellij übergeben
 
         Date date = new Date();
         long time = date.getTime();
         Timestamp ts = new Timestamp(time);
         String timestamp = ts.toString();
         System.out.println("Local start time is " + timestamp);
-        Daytime remote = (Daytime) Naming.lookup("//" + host + "/localhost");
+        Daytime remote = (Daytime) Naming.lookup("//" + host + "/daytime");
         String received = remote.getDaytime();
         System.out.println("Received time is " + received);
         time = date.getTime();
